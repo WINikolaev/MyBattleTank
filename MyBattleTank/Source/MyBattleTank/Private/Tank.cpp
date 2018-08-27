dfc87f42487aa627b9e0c06dba93f2073a260f83
WIINnikolaev <https://github.com/WINikolaev>
2018-08-27 21:05:57 +0300

    no message

--- MyBattleTank/Source/MyBattleTank/Private/Tank.cpp
+++ MyBattleTank/Source/MyBattleTank/Private/Tank.cpp
@@ -15,8 +15,6 @@ ATank::ATank()
 	PrimaryActorTick.bCanEverTick = false;
 
 	TankAimingComponent = CreateDefaultSubobject<UTankAimingComponent>(FName("AimingComponent"));
-	TankMovementComponent = CreateDefaultSubobject<UTankMovementComponent>(FName("MovementComponent"));
-
 }
 
 void ATank::SetBarrelReference(UTankBarrel * BarrelToSet)
